9f9d5df8c562d107582f1832ab460fdc838ed522
Angelo <gabriel_propio95@hotmail.com>
2023-09-21 11:40:09 -0400

    formulario de creacion de producto validado, campos correctos y añadido un modal para mensaje al usuario + todo ok al guardar en la base de datos y storage

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,86 +1,150 @@
-import appFirebase from '../credenciales'
-import {getFirestore, collection, addDoc} from 'firebase/firestore'
-import {getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
-import { useState } from 'react'
+import appFirebase from "../credenciales";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useState } from "react";
 
-const db = getFirestore(appFirebase)
-const storage = getStorage(appFirebase)
+const db = getFirestore(appFirebase);
+const storage = getStorage(appFirebase);
 
 export default function CreateForm() {
-    let urlImDesc;
-    const [habilitar, setHabilitar] = useState(true)
-    
-    //funcion para guardar comida
-    const guardarProduct = async(e)=>{
-        e.preventDefault();
-        const nombre = e.target.nombre.value;
-        const precio = e.target.precio.value;
+  //let urlImDesc;
+  const [habilitar, setHabilitar] = useState(false);
+  const [urlImg, setUrlImg] = useState();
 
-        const newComida = {
-            nombre : nombre,
-            precio: precio,
-            imagen: urlImDesc
-        }
-        console.log(newComida);
-    
-        // try {
-        //     await addDoc(collection(db,'comidas'),{
-        //         ...newComida
-        //     })
-        // } catch (error) {
-        //     console.log(error);
-        //     alert(error);
-        // }
-        //reseteo de variables en el formulario
-        e.target.nombre.value = ''
-        e.target.precio.value = ''
-        e.target.file.value = ''
-        setHabilitar(true)
-    }
+  //funcion para guardar comida
+  const guardarProduct = async (e) => {
+    e.preventDefault();
+    const nombre = e.target.nombre.value;
+    const precio = e.target.precio.value;
 
-    const fileHandler = async(e)=>{
-        //detecta el archivo
-        const archivoL = e.target.files[0];
-        //cargarlo al storage
-        const refArchivo = ref(storage, `productos/${archivoL.name}`);
-        await uploadBytes(refArchivo, archivoL)
-        //obtener la url de la imagen
-        urlImDesc = await getDownloadURL(refArchivo)
-        // if (urlImDesc) {
-        //     alert("Imagen cargada")
-        //     setHabilitar(true)
-        // }
- 
+    const newComida = {
+      nombre: nombre,
+      precio: precio,
+      imagen: urlImg,
+    };
+    console.log(newComida);
+    try {
+      await addDoc(collection(db, "comidas"), {
+        ...newComida,
+      });
+    } catch (error) {
+      console.log(error);
+      alert(error);
     }
+    //reseteo de variables en el formulario
+    e.target.nombre.value = "";
+    e.target.precio.value = "";
+    e.target.file.value = "";
+    setHabilitar(false);
+  };
+
+  const fileHandler = async (e) => {
+    try {
+      //detecta el archivo
+      const archivoL = e.target.files[0];
+      //cargarlo al storage
+      const refArchivo = ref(storage, `productos/${archivoL.name}`);
+      await uploadBytes(refArchivo, archivoL);
+      //obtener la url de la imagen
+      const url = await getDownloadURL(refArchivo);
+      setUrlImg(url);
+      alert("imagen cargada con exito");
+      setHabilitar(true);
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <div className="row">
-        
-        <div className="col-md-5">
-        <div className='card card-body'>
-            <h3 className='text-center'>Agregar comida</h3>
-            <form onSubmit={guardarProduct}>
-                <label>Nombre:</label>
-                <div className='form-group'>
-                    <input type="text" placeholder='ingresar el nombre de la comida' id='nombre'  className='form-control mt-1' required />
-                </div>
-                <label className='form-label mt-3'>Precio:</label>
-                <div className='form-group'>
-                    <input type="number" placeholder='ingresar precio de la comida' id='precio'  className='form-control' required />
-                </div>
+      <div className="col-md-5">
+        <div className="card card-body">
+          <h3 className="text-center">Agregar comida</h3>
+          <form onSubmit={guardarProduct}>
+            <label>Nombre:</label>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="ingresar el nombre de la comida"
+                id="nombre"
+                className="form-control mt-1"
+                required
+              />
+            </div>
+            <label className="form-label mt-3">Precio:</label>
+            <div className="form-group">
+              <input
+                type="number"
+                placeholder="ingresar precio de la comida"
+                id="precio"
+                className="form-control"
+                required
+              />
+            </div>
 
-                <label className='form-label mt-3'>Agregar Imagen:</label>
-                <input type="file" name="" id="file" placeholder='Agregar la imagen' className='form-control' onChange={fileHandler}  />
+            <label className="form-label mt-3">Agregar Imagen:</label>
+            <input
+              type="file"
+              name=""
+              id="file"
+              placeholder="Agregar la imagen"
+              className="form-control"
+              onChange={fileHandler}
+              required
+            />
 
-                <button className={habilitar ? 'btn btn-primary mt-3 form-control' : 'btn btn-primary mt-3 form-control disabled'}>{habilitar ? "Guardar Producto" : "Llena los campos"}</button>
-            </form>
+            <button
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+              className={
+                habilitar
+                  ? "btn btn-primary mt-3 form-control"
+                  : "btn btn-primary mt-3 form-control disabled"
+              }
+            >
+              {habilitar ? "Guardar Producto" : "Llena los campos"}
+            </button>
+          </form>
         </div>
+        {/* <!-- Modal --> */}
+        <div
+          className="modal fade"
+          id="staticBackdrop"
+          data-bs-backdrop="static"
+          data-bs-keyboard="false"
+          tabIndex="-1"
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                  Ventana de alerta
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">Producto guardado correctamente</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  data-bs-dismiss="modal"
+                >
+                  Aceptar
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div className="col-md-7">
-            tabla de productos
-        </div>
-        
+      <div className="col-md-7">tabla de productos</div>
     </div>
-  )
+  );
 }
