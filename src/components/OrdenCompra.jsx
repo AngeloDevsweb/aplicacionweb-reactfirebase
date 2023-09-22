26511c8468e687db754f9f58ad0d20fa9a1cbf89
Angelo <gabriel_propio95@hotmail.com>
2023-09-22 13:53:46 -0400

    creacion de pdf completa, guardado de pdf automatico y visualizacion automatica, fecha formateada para visualizacion en pdf ok funciones añadidas ok

--- src/components/OrdenCompra.jsx
+++ src/components/OrdenCompra.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { Store } from "../context/StoreContext";
+import {jsPDF} from 'jspdf'
+import "jspdf-autotable"
 
 import appFirebase from "../credenciales";
 import { getFirestore, addDoc, collection } from "firebase/firestore";
@@ -13,6 +15,7 @@ export default function OrdenCompra() {
 
   const subtotal = cartItems.reduce((a, c) => a + c.quantity * c.precio, 0);
   const productTotal = cartItems;
+  const fecha = new Date();
 
   const delToCart = (item) => {
     dispatch({ type: "CART_REMOVE_ITEM", payload: item });
@@ -20,18 +23,53 @@ export default function OrdenCompra() {
 
   //esta funcion sera para guardar la info en la base de datos
   const saveSale = async () => {
-    const fecha = new Date();
     try {
       await addDoc(collection(db, "ventas"), {
         ...productTotal,
         fecha,
         subtotal,
       });
+      generatePdf()
     } catch (error) {
       alert(error);
     }
     dispatch({ type: "REMOVE_CART" });
     alert("guardado con exito");
+  };
+
+  const generatePdf = ()=>{
+    const doc = new jsPDF
+    const formatDate = formatearFecha(fecha)
+    doc.text('FACTURA', 95,20)
+
+    //crear una tabla para los datos de la factura
+    const columns = ['Fecha', 'Nombre', 'Precio','Cantidad', 'total']
+    const data = productTotal.map(products => [`${formatDate}`, products.nombre, products.precio, products.quantity, subtotal])
+
+    doc.autoTable({
+        startY:30,
+        head:[columns],
+        body: data
+    })
+
+    //si quiero mostrar debajo un subtotal
+    doc.text(`Total: ${subtotal}$`, 30, 100)
+      // Obtener los datos del PDF como Blob
+      const pdfData = doc.output('blob');
+
+      // Crear una URL para el Blob
+      const pdfUrl = URL.createObjectURL(pdfData);
+
+      //guardar el pdf con un nombre especifico
+      doc.save(`factura_${formatDate}.pdf`)
+  
+           // Abrir el PDF en una nueva ventana
+      window.open(pdfUrl, '_blank');
+  }
+//   funcion para formatear fecha
+  const formatearFecha = (fecha) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return fecha.toLocaleDateString('es-ES', options);
   };
 
   return (
